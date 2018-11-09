2f2b393032bbee29483647884235ebf40feeba2b
Abdourahamane LY <abdourahamane.ly1@gmail.com>
2018-11-10 00:33:42 +0100

    angular material and adding toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import { UtilisateurComponent } from './utilisateur/utilisateur.component';
+import { ActiviteComponent } from './activite/activite.component';
+
+const ROUTES: Routes = [
+  // {path: 'home', component: AppComponent},
+  // {path: '', redirectTo: 'home', pathMatch: 'full'},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UtilisateurComponent,
+    ActiviteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule, 
+
+    [RouterModule.forRoot(ROUTES)],
+
+    //MATERIAL
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
